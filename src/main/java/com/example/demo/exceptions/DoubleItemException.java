4f8f07647ab9146dcbefcaa1029826b5dae6e6b9
beatajb <orchidia@o2.pl>
2021-01-07 02:09:02 +0100

    Fixed typo

--- src/main/java/com/example/demo/exceptions/DoubleItemException.java
+++ src/main/java/com/example/demo/exceptions/DoubleItemException.java
@@ -7,6 +7,6 @@ public class DoubleItemException extends IllegalArgumentException {
 	private static final long serialVersionUID = 1L;
 
 	public DoubleItemException(Long id, @NonNull String name) {
-        super("Item with name: " + name+ " exists already and it's id is: "+id);
+        super("Item with name: " + name+ " exists already and its id is: "+id);
     }
 }
